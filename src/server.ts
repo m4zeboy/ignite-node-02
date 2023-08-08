2b4042a612812da45005c274b62533f6413b86ff
m4zeboy <moises775azevedo@gmail.com>
2023-08-08 11:35:26 -0300

    Tratando env com Zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import fastify from "fastify";
 import { knex } from "./database";
-import crypto from "node:crypto";
+import { env } from "./env";
 
 const server = fastify();
 
@@ -9,6 +9,6 @@ server.get("/hello", async () => {
   return transactions;
 });
 
-server.listen({ port: 3333 }).then(() => {
+server.listen({ port: env.PORT }).then(() => {
   console.log("server running");
 });
