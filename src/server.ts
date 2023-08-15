ef638d63552dc1155f572654a61818235b96c643
m4zeboy <moises775azevedo@gmail.com>
2023-08-15 16:31:18 -0300

    Criando primeiro teste

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,14 @@ import { transactionRoutes } from "./routes/transactions";
 
 const server = fastify();
 
+/* 
+  Testes unitários: Testar uma unidade isolada da aplicação
+  Testes de integração: Testar a comunicação entre duas ou mais unidades da aplicação
+  Testes E2E - Ponta a ponta: Simulam um usuário operando a nossa aplicação
+
+  Pirâmide de teste
+*/
+
 server.register(cookie);
 
 server.register(transactionRoutes, {
---
+++ test/example.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "vitest";
+
+test("o usuario consegue criar uma nova transação", () => {
+  const responseStatusCode = 201;
+  expect(responseStatusCode).toEqual(201);
+});
